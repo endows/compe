9f4de544984810f2c05c51c8abb19d4324ba06f8
takashi <endotakashi1992@gmail.com>
2015-10-26 11:44:18 +0900

    competionCollection

--- Join.jsx
+++ Join.jsx
@@ -1,11 +1,19 @@
 Join = React.createClass({
+  getInitialState: function() {
+    return {friend_code: '',trainer_name:''};
+  },
+  _register:function(){
+    let friend_code = this.refs.friend_code.getDOMNode().value
+    let trainer_name = this.refs.trainer_name.getDOMNode().value
+    Meteor.call('registFriendCode',{friend_code:friend_code,trainer_name:trainer_name})
+  },
     render: function() {
         return (
             <div>
               <div>
-                <input placeholder="0000-0000-0000"></input>@
-                <input placeholder="トレーナー名"></input>
-                <button>参加</button>
+                <input type='tel' ref='friend_code' placeholder="0000-0000-0000"></input>@
+                <input ref='trainer_name' placeholder="トレーナー名"></input>
+                <button onClick={this._register}>参加</button>
               </div>
             </div>
         );
--- server/actions.js
+++ server/actions.js
@@ -1,8 +1,11 @@
 Meteor.methods({
   'registFriendCode':function(obj){
-    Meteor.users.update({_id:this.userId},{
+    Meteor.users.update({_id:this.userId},{$set:
+    {
       friend_code:obj.friend_code,
       trainer_name:obj.trainer_name
+    }
+
     })
   }
 })
